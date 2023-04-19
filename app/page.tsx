fb25414aada07a95c69ada5c35aef99517bbf8f8
Ethan Mick <ethan@ethanmick.com>
2023-04-19 13:56:06 -0400

    fix: Remove Inter
    It's nice, but distracting in the code.

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import clsx from 'clsx'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={clsx(
-        'mx-auto container flex justify-center mt-20',
-        inter.className
-      )}
-    >
+    <main className="mx-auto container flex justify-center mt-20">
       <Button onClick={() => console.log('Boom!')}>Button</Button>
     </main>
   )
